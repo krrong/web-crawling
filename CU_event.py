2e8c1536c3079a01b7786ec358ed36aad5ef3db2
krrong <khk9664@naver.com>
2022-02-28 23:54:14 +0900

    편의점 행사 상품 받아오기
    1+1 상품, 2+1 상품을 가져와 파일로 저장하도록 코드를 수정하였다.
    현재는 1+1 상품 혹은 2+1 상품을 저장하는 코드 중 하나를 주석 처리 한 뒤 실행해야 한다.

--- CU_event.py
+++ CU_event.py
@@ -7,52 +7,109 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# interval = 3
+# browser = webdriver.Chrome("./webscraping_basic/chromedriver.exe")
+# url = "https://cu.bgfretail.com/event/plus.do?category=event"
+# browser.get(url)
+
+# # 1+1 버튼 클릭
+# time.sleep(interval)
+# elem = browser.find_element_by_class_name("eventInfo").find_element_by_link_text("1+1").click()
+# time.sleep(interval)
+
+# while True:
+#     time.sleep(interval)
+#     try:
+#         # 더보기 버튼 받아오기
+#         elem = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='contents']/div[1]/div[2]/div/div[1]/a")))
+#     # 더보기 버튼 없으면 종료    
+#     except Exception:
+#         print("타임 아웃")
+#         break
+        
+#     # 더보기 버튼 클릭
+#     elem.click()
+
+# soup = BeautifulSoup(browser.page_source, "lxml")
+# goods = soup.find_all("p", attrs={"class":"prodName"})      # 전체 상품
+# prices = soup.find_all("p", attrs={"class":"prodPrice"})    # 전체 상품 가격
+# images = soup.find_all("img", attrs={"width":180})          # 전체 상품 이미지
+
+# # # 각 행사 상품 이미지 파일로 저장
+# # for idx, image in enumerate(images):
+# #     image_url = image["src"]
+    
+# #     image_res = requests.get(image_url)
+# #     image_res.raise_for_status()
+    
+# #     with open("img_1+1_{}.jpg".format(idx), "wb") as f:
+# #         f.write(image_res.content)
+        
+# #     if idx==3:
+# #         break
+
+# # 각 행사 상품 이미지 경로 저장
+# for idx, image in enumerate(images):
+#     image_url = image["src"]
+    
+#     with open("cu_11_link.txt", "w", encoding='utf-8') as f:
+#         f.write(image_url + "\n")
+        
+# print("경로 저장 완료")
+
+
+# for idx, good in enumerate(goods):
+#     # 상품 이름 : 가격 출력
+#     print(good.get_text() + " : " + prices[idx].get_text())
+    
+#     # 상품 이름 저장
+#     with open('cu_11_name.txt','w', encoding='utf-8') as f1:
+#         f1.write(good.get_text() + '\n')
+    
+#     # 상품 가격 저장
+#     with open('cu_11_price.txt','w', encoding='utf-8') as f2:
+#         f2.write(prices[idx].get_text() + '\n')
+        
+
+# print("저장 완료")
+
+# # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# 2+1 상품
+
 interval = 3
 browser = webdriver.Chrome("./webscraping_basic/chromedriver.exe")
 url = "https://cu.bgfretail.com/event/plus.do?category=event"
 browser.get(url)
 
-# 1+1 버튼 클릭
+# 2+1 버튼 클릭
+elem = browser.find_element_by_class_name("eventInfo").find_element_by_link_text("2+1").click()
 time.sleep(interval)
-elem = browser.find_element_by_class_name("eventInfo").find_element_by_link_text("1+1").click()
-time.sleep(interval)
-
+interval = 15
 while True:
     time.sleep(interval)
     try:
         # 더보기 버튼 받아오기
         elem = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='contents']/div[1]/div[2]/div/div[1]/a")))
-    # 더보기 버튼 없으면 종료    
-    except Exception:
-        print("타임 아웃")
+    except:
         break
-        
+    
+    # 더보기 버튼이 없으면 종료
+    if not elem:
+        break
+    
     # 더보기 버튼 클릭
     elem.click()
 
 soup = BeautifulSoup(browser.page_source, "lxml")
-goods = soup.find_all("p", attrs={"class":"prodName"})      # 전체 상품
-prices = soup.find_all("p", attrs={"class":"prodPrice"})    # 전체 상품 가격
-images = soup.find_all("img", attrs={"width":180})          # 전체 상품 이미지
-
-# # 각 행사 상품 이미지 파일로 저장
-# for idx, image in enumerate(images):
-#     image_url = image["src"]
-    
-#     image_res = requests.get(image_url)
-#     image_res.raise_for_status()
-    
-#     with open("img_1+1_{}.jpg".format(idx), "wb") as f:
-#         f.write(image_res.content)
-        
-#     if idx==3:
-#         break
+goods = soup.find_all("p", attrs={"class":"prodName"})    # 상품
+prices = soup.find_all("p", attrs={"class":"prodPrice"})  # 가격
+images = soup.find_all("img", attrs={"width":180})          # 이미지
 
 # 각 행사 상품 이미지 경로 저장
 for idx, image in enumerate(images):
     image_url = image["src"]
     
-    with open("cu_11_url.txt", "a") as f:
+    with open("cu_21_link.txt", "w", encoding='utf-8') as f:
         f.write(image_url + "\n")
         
 print("경로 저장 완료")
@@ -63,48 +120,11 @@ for idx, good in enumerate(goods):
     print(good.get_text() + " : " + prices[idx].get_text())
     
     # 상품 이름 저장
-    with open('cu_11_name.txt','a') as f1:
+    with open('cu_21_name.txt','w', encoding='utf-8') as f1:
         f1.write(good.get_text() + '\n')
     
     # 상품 가격 저장
-    with open('cu_11_price.txt','a') as f2:
+    with open('cu_21_price.txt','w', encoding='utf-8') as f2:
         f2.write(prices[idx].get_text() + '\n')
-        
 
 print("저장 완료")
-
-# # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# # 2+1 버튼 클릭
-# elem = browser.find_element_by_class_name("eventInfo").find_element_by_link_text("2+1").click()
-# time.sleep(interval)
-# interval = 15
-# while True:
-#     time.sleep(interval)
-#     # 더보기 버튼 받아오기
-#     elem = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='contents']/div[1]/div[2]/div/div[1]/a")))
-    
-#     # 더보기 버튼이 없으면 종료
-#     if not elem:
-#         break
-    
-#     # 더보기 버튼 클릭
-#     elem.click()
-
-# soup = BeautifulSoup(browser.page_source, "lxml")
-# goods = soup.find_all("p", attrs={"class":"prodName"})    # 상품
-# prices = soup.find_all("p", attrs={"class":"prodPrice"})  # 가격
-# images = soup.find_all("img", attrs={"width":180})          # 이미지
-
-# # 각 행사 상품 이미지 파일로 저장
-# for idx, image in enumerate(images):
-#     image_url = image["src"]
-    
-#     image_res = requests.get(image_url)
-#     image_res.raise_for_status()
-    
-#     with open("img_2+1_{}.jpg".format(idx), "wb") as f:
-#         f.write(image_res.content)
-
-# # 상품 이름 : 가격 출력
-# for idx, good in enumerate(goods):
-#     print(good.get_text() + " : " + prices[idx].get_text())
--- Seven_event.py
+++ Seven_event.py
@@ -8,6 +8,83 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# interval = 2
+# browser = webdriver.Chrome("./webscraping_basic/chromedriver.exe")
+# browser.maximize_window()   # 페이지 크기 최대화
+
+# # 페이지 이동
+# url = "https://www.7-eleven.co.kr/product/presentList.asp"
+# browser.get(url)
+
+# # "+"버튼 찾아서 클릭(이때의 xpath는 아래와 같음)
+# time.sleep(interval)
+# elem = browser.find_element_by_xpath("//*[@id='listUl']/li[15]/a")
+# elem.click()
+# time.sleep(interval)
+
+# # 현재 문서 높이를 가져와서 저장
+# prev_height = browser.execute_script("return document.body.scrollHeight")
+
+# # # 1+1 페이지 끝까지 내리기
+# # while True:
+# #     # +버튼 찾기
+# #     elem = browser.find_element_by_xpath("//*[@id='moreImg']/a")
+# #     # +버튼 클릭
+# #     elem.send_keys(Keys.ENTER)
+# #     # 페이지 로딩 대기
+# #     time.sleep(interval)
+
+# #     print("이전 높이 : ", prev_height)
+
+# #     # 현재 문서 높이를 가져와서 저장
+# #     curr_height = browser.execute_script("return document.body.scrollHeight")
+
+# #     # 높이가 같으면 +버튼이 눌리지 않는 것이므로 종료
+# #     if prev_height == curr_height:
+# #         break
+    
+# #     print("이후 높이 : ", curr_height)
+    
+# #     # 이전 높이 갱신
+# #     prev_height = curr_height
+    
+# #      # 페이지 로딩 대기
+# #     time.sleep(interval)
+    
+# # soup = BeautifulSoup(browser.page_source, "lxml")
+# # goods = soup.find_all("div", attrs={"class":"name"})      # 전체 상품
+# # prices = soup.find_all("div", attrs={"class":"price"})    # 전체 상품 가격
+# # images = soup.find_all("img", attrs={"height":130})          # 전체 상품 이미지
+
+
+# # # 각 행사 상품 이미지 경로 저장
+# # for idx, image in enumerate(images):
+# #     image_url = image["src"]
+    
+# #     with open("seven_11_link.txt", "w", encoding='utf-8') as f:
+# #         f.write("https://www.7-eleven.co.kr/" + image_url + "\n")
+        
+# # print("경로 저장 완료")
+
+
+# # for idx, good in enumerate(goods):
+# #     # 상품 이름 : 가격 출력
+# #     print(good.get_text() + " : " + prices[idx].get_text())
+    
+# #     # 상품 이름 저장
+# #     with open('seven_11_name.txt','w', encoding='utf-8') as f1:
+# #         f1.write(good.get_text() + '\n')
+    
+# #     # 상품 가격 저장
+# #     with open('seven_11_price.txt','w', encoding='utf-8') as f2:
+# #         f2.write(prices[idx].get_text()[1:-1] + '\n')
+        
+# # print("Seven 1+1 상품명, 가격, url 저장 완료")
+
+
+# ----------------------------------------------------------------
+# 2+1 상품
+
 interval = 2
 browser = webdriver.Chrome("./webscraping_basic/chromedriver.exe")
 browser.maximize_window()   # 페이지 크기 최대화
@@ -15,74 +92,6 @@ browser.maximize_window()   # 페이지 크기 최대화
 # 페이지 이동
 url = "https://www.7-eleven.co.kr/product/presentList.asp"
 browser.get(url)
-
-# "+"버튼 찾아서 클릭(이때의 xpath는 아래와 같음)
-time.sleep(interval)
-elem = browser.find_element_by_xpath("//*[@id='listUl']/li[15]/a")
-elem.click()
-time.sleep(interval)
-
-# 현재 문서 높이를 가져와서 저장
-prev_height = browser.execute_script("return document.body.scrollHeight")
-
-# 1+1 페이지 끝까지 내리기
-while True:
-    # +버튼 찾기
-    elem = browser.find_element_by_xpath("//*[@id='moreImg']/a")
-    # +버튼 클릭
-    elem.send_keys(Keys.ENTER)
-    # 페이지 로딩 대기
-    time.sleep(interval)
-
-    print("이전 높이 : ", prev_height)
-
-    # 현재 문서 높이를 가져와서 저장
-    curr_height = browser.execute_script("return document.body.scrollHeight")
-
-    # 높이가 같으면 +버튼이 눌리지 않는 것이므로 종료
-    if prev_height == curr_height:
-        break
-    
-    print("이후 높이 : ", curr_height)
-    
-    # 이전 높이 갱신
-    prev_height = curr_height
-    
-     # 페이지 로딩 대기
-    time.sleep(interval)
-    
-soup = BeautifulSoup(browser.page_source, "lxml")
-goods = soup.find_all("div", attrs={"class":"name"})      # 전체 상품
-prices = soup.find_all("div", attrs={"class":"price"})    # 전체 상품 가격
-images = soup.find_all("img", attrs={"height":130})          # 전체 상품 이미지
-
-
-# 각 행사 상품 이미지 경로 저장
-for idx, image in enumerate(images):
-    image_url = image["src"]
-    
-    with open("seven_11_url.txt", "a") as f:
-        f.write("https://www.7-eleven.co.kr/" + image_url + "\n")
-        
-print("경로 저장 완료")
-
-
-for idx, good in enumerate(goods):
-    # 상품 이름 : 가격 출력
-    print(good.get_text() + " : " + prices[idx].get_text())
-    
-    # 상품 이름 저장
-    with open('seven_11_name.txt','a') as f1:
-        f1.write(good.get_text() + '\n')
-    
-    # 상품 가격 저장
-    with open('seven_11_price.txt','a') as f2:
-        f2.write(prices[idx].get_text()[1:-1] + '\n')
-        
-print("Seven 1+1 상품명, 가격, url 저장 완료")
-
-
-# ----------------------------------------------------------------
 
 # 2+1 버튼
 elem = browser.find_element_by_link_text("2+1")
@@ -100,8 +109,12 @@ prev_height = browser.execute_script("return document.body.scrollHeight")
 index = 0
 # 2+1 페이지 끝까지 내리기
 while True:
-    # +버튼 찾기
-    elem = browser.find_element_by_xpath("//*[@id='moreImg']/a")
+    try:
+        # +버튼 찾기
+        elem = browser.find_element_by_xpath("//*[@id='moreImg']/a")
+    
+    except:
+        break
     
     if not elem:
         break
@@ -142,7 +155,7 @@ images = soup.find_all("img", attrs={"height":130})          # 전체 상품 이
 for idx, image in enumerate(images):
     image_url = image["src"]
     
-    with open("seven_11_url.txt", "a") as f:
+    with open("seven_21_link.txt", "w", encoding='utf-8') as f:
         f.write("https://www.7-eleven.co.kr/" + image_url + "\n")
         
 print("경로 저장 완료")
@@ -153,11 +166,11 @@ for idx, good in enumerate(goods):
     print(good.get_text() + " : " + prices[idx].get_text())
     
     # 상품 이름 저장
-    with open('seven_11_name.txt','a') as f1:
+    with open('seven_21_name.txt','w', encoding='utf-8') as f1:
         f1.write(good.get_text() + '\n')
     
     # 상품 가격 저장
-    with open('seven_11_price.txt','a') as f2:
-        f2.write(prices[idx].get_text()[1:-1] + '\n')
+    with open('seven_21_price.txt','w', encoding='utf-8') as f2:
+        f2.write(prices[idx].get_text()[1:-1] + "원" + '\n')
         
-print("Seven 2+1 상품명, 가격, url 저장 완료")
+print("Seven 2+1 상품명, 가격, link 저장 완료")
